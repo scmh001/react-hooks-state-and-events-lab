cad5876796df40abadc88dce7df42c0626398704
Shukri Hussein <husseinshukri@outlook.com>
2024-03-13 11:28:02 -0400

    lab completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,40 @@
+// import React from "react";
+
+// function Item({ name, category }) {
+//   return (
+//     <li className="">
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button className="add">Add to Cart</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
+
+
+
+
+
+//----------------------------------------------------------------------
+
+
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function handleClick() {
+    setIsInCart(!isInCart);
+  }
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={isInCart ? "remove" : "add"}>
+        {isInCart ? "Remove from cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
